7b87dc07975eddf2cfd1ade0fe6955f9b61a62ed
Sesha Mohan Srivatsava Maddali <87626654+SrivatsavaMaddali@users.noreply.github.com>
2023-06-23 23:26:04 +0530

    Time: 554 ms (57.76%), Space: 0B (100.00%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,10 +1,15 @@
 # Write your MySQL query statement below
-select name from Employee
-where id in (
-    select managerId 
-    from (
-    select managerId, count(id) as cnt
-from Employee
-    group by managerId ) as a
-    where a.cnt>=5
+# select name from Employee
+# where id in (
+#     select managerId 
+#     from (
+#     select managerId, count(id) as cnt
+# from Employee
+#     group by managerId ) as a
+#     where a.cnt>=5
+# )
+Select Name from Employee where Id in
+(
+    Select ManagerId from Employee 
+    group by ManagerId having count(*) >= 5
 )
