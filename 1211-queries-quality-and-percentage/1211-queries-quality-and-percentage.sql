6fd661c70ee8e42329e7556605d8f08955ce23df
Sesha Mohan Srivatsava Maddali <87626654+SrivatsavaMaddali@users.noreply.github.com>
2023-06-29 01:22:44 +0530

    Time: 1383 ms (53.68%), Space: 0B (100.00%) - LeetHub

--- 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
+++ 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -1,4 +1,4 @@
-# # Write your MySQL query statement below
+# Write your MySQL query statement below
 select query_name, 
 ROUND(AVG(rating / position), 2) AS quality,
 round(SUM(rating<3)/count(query_name)*100,2) as poor_query_percentage 
