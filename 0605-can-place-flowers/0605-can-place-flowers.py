5d49c1107ac9c04f873366bb4e70214e4a561b80
Sesha Mohan Srivatsava Maddali <87626654+SrivatsavaMaddali@users.noreply.github.com>
2023-06-30 01:31:36 +0530

    Time: 178 ms (44.71%), Space: 16.7 MB (95.29%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.py
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -3,9 +3,8 @@ class Solution:
         if n==0:
             return True
         cnt=0
-        m=len(flowerbed)
-        for i in range(m):
-            if flowerbed[i]==0 and (i==0 or flowerbed[i-1]!=1) and (i==m-1 or flowerbed[i+1]!=1):
+        for i in range(len(flowerbed)):
+            if flowerbed[i]==0 and (i==0 or flowerbed[i-1]!=1) and (i==len(flowerbed)-1 or flowerbed[i+1]!=1):
                 cnt+=1
                 flowerbed[i]=1
         return True if cnt>=n else False
