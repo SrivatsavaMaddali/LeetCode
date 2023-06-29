57584b85fff41ba76a3564c60edabec8f0098439
Sesha Mohan Srivatsava Maddali <87626654+SrivatsavaMaddali@users.noreply.github.com>
2023-06-30 00:38:22 +0530

    Time: 46 ms (72.11%), Space: 16.1 MB (98.04%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
@@ -4,10 +4,6 @@ class Solution:
         l1, l2 = len(str1), len(str2)
         gl = math.gcd(l1, l2)
         m = str1[:gl]
-        # if self.is_divisor(str1, m) and self.is_divisor(str2, m):
-        #     return m
         if str1==m*(len(str1)//len(m)) and str2==m*(len(str2)//len(m)):
             return m
         return ""
-    # def is_divisor(self, s, d):
-    #     return s == d * (len(s) // len(d))
