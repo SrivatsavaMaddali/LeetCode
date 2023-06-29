f20f1a967bbd4ff6eddbaa063e56f78eb7308297
Sesha Mohan Srivatsava Maddali <87626654+SrivatsavaMaddali@users.noreply.github.com>
2023-06-30 01:27:16 +0530

    Time: 167 ms (85.16%), Space: 16.7 MB (67.62%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.py
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -3,21 +3,13 @@ class Solution:
         i=0
         if n==0:
             return True
-        # if n>ceil(len(flowerbed)/2):
-        #     return False
-        # if len(flowerbed) == 1:
-        #     return True if n==1 and flowerbed[0] == 0 else False
-        # if len(flowerbed) == 1:
-        #     return True if n==0 and flowerbed[0] == 1 else False
-        # if len(flowerbed) == 2:
-        #     return True if flowerbed== [0,0] else False
         cnt=0
         for i in range(len(flowerbed)):
             if flowerbed[i]==0 and (i==0 or flowerbed[i-1]!=1) and (i==len(flowerbed)-1 or flowerbed[i+1]!=1):
                 cnt+=1
                 flowerbed[i]=1
-        if cnt>=n:
-            return True
-        else:
-            return False
-
+        # if cnt>=n:
+        #     return True
+        # else:
+        #     return False
+        return True if cnt>=n else False
