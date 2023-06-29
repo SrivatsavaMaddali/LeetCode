9700245b3bb208dfa5ed458299b7d4f1c92c9ad1
Sesha Mohan Srivatsava Maddali <87626654+SrivatsavaMaddali@users.noreply.github.com>
2023-06-30 02:33:43 +0530

    Time: 42 ms (88.47%), Space: 16.4 MB (61.19%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
@@ -1,20 +1,28 @@
+# class Solution:
+#     def reverseWords(self, s: str) -> str:
+#         s.strip()
+#         words=[]
+#         start=0
+#         wc=0
+#         for i in range(len(s)):
+#             if s[i]==' ':
+#                 wc+=1
+#                 words.append(s[start:i].strip())
+#                 start=i
+#         words.append(s[start:len(s)] if wc==0 else s[start+1:len(s)])
+#         nice=[]
+#         for y in words:
+#             if y!='':
+#                 nice.append(y)
+#         words=nice
+#         result=""
+#         for x in words[::-1]:
+#             result+=x+" "
+#         result=result.strip()
+#         return result
 class Solution:
     def reverseWords(self, s: str) -> str:
-        s.strip()
-        words=[]
-        start=0
-        wc=0
-        for i in range(len(s)):
-            if s[i]==' ':
-                wc+=1
-                words.append(s[start:i].strip())
-                start=i
-        words.append(s[start:len(s)] if wc==0 else s[start+1:len(s)])
-        nice=[]
-        for y in words:
-            if y!='':
-                nice.append(y)
-        words=nice
+        words=s.split()
         result=""
         for x in words[::-1]:
             result+=x+" "
