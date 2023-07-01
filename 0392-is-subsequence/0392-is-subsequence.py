da2d9ebf04c725f6cd636ccdbef67caf4b71ace9
Sesha Mohan Srivatsava Maddali <87626654+SrivatsavaMaddali@users.noreply.github.com>
2023-07-02 01:16:20 +0530

    Time: 57 ms (12.45%), Space: 16.3 MB (63.76%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -6,8 +6,6 @@ class Solution:
             return False
         if len(s)==len(t) and s!=t:
             return False
-        if len(s)==1 and s[0] in t:
-            return True
         i,j=0,0
         while i<len(s) and j<len(t):
             if t[j]==s[i]:
