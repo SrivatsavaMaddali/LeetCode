d9d5bfca2d514449f862161e785d9162196f220c
Sesha Mohan Srivatsava Maddali <87626654+SrivatsavaMaddali@users.noreply.github.com>
2023-07-04 21:36:59 +0530

    Time: 1067 ms (56.21%), Space: 0B (100.00%) - LeetHub

---
+++ 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
@@ -0,0 +1,14 @@
+SELECT ROUND(t1.cnt1 / t2.cnt2, 2) AS fraction
+
+FROM
+
+  (SELECT COUNT(distinct a1.player_id) AS cnt1
+  FROM Activity a1
+  JOIN Activity a2 ON a1.player_id = a2.player_id
+  WHERE DATEDIFF(a2.event_date, a1.event_date) = 1 and 
+  (a1.player_id,a1.event_date) IN (select player_id,min(event_date)
+     from Activity
+     group by player_id)) t1,
+  
+  (SELECT COUNT(DISTINCT player_id) AS cnt2
+  FROM Activity) t2;
