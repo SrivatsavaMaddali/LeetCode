b341d0704aaeb7a448deb0379de04da878f16f64
Sesha Mohan Srivatsava Maddali <87626654+SrivatsavaMaddali@users.noreply.github.com>
2023-07-04 21:38:44 +0530

    Time: 971 ms (81.55%), Space: 0B (100.00%) - LeetHub

--- 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
+++ 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
@@ -5,10 +5,12 @@ FROM
   (SELECT COUNT(distinct a1.player_id) AS cnt1
   FROM Activity a1
   JOIN Activity a2 ON a1.player_id = a2.player_id
-  WHERE DATEDIFF(a2.event_date, a1.event_date) = 1 and 
+  WHERE DATEDIFF(a2.event_date, a1.event_date) = 1 #players with diff in login dates=1
+   and 
   (a1.player_id,a1.event_date) IN (select player_id,min(event_date)
      from Activity
-     group by player_id)) t1,
+     group by player_id)) t1#and the first date is the first login of the player
+     ,
   
   (SELECT COUNT(DISTINCT player_id) AS cnt2
-  FROM Activity) t2;
+  FROM Activity) t2;#This query is to count the number of players
