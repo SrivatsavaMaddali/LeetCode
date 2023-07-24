777fc3ed67e66a159d391cc4dd60a7ec3f12ae18
Sesha Mohan Srivatsava Maddali <87626654+SrivatsavaMaddali@users.noreply.github.com>
2023-07-24 22:10:06 +0530

    Time: 69 ms (50.00%), Space: 16.2 MB (100.00%) - LeetHub

--- 2784-check-if-array-is-good/2784-check-if-array-is-good.py
+++ 2784-check-if-array-is-good/2784-check-if-array-is-good.py
@@ -1,8 +1,19 @@
 class Solution:
     def isGood(self, nums: List[int]) -> bool:
-        l=len(nums)
-        good=[]
-        for i in range(1,l):
-            good.append(i)
-        good.append(l-1)
-        return sorted(nums)==good
+        l = len(nums)
+        d = {}
+        for x in range(1,l):
+            d[x] = 0
+        for x in nums:
+            if x>l-1:
+                return False
+            d[x] += 1
+        print(d)
+        if len(d)!=l-1:
+            return False
+        for x in range(1, l - 1):
+            if d[x]!= 1:
+                return False
+        if d[l - 1] != 2:
+            return False
+        return True
