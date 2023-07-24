f63cc6956c1e828824439bdef36e22a834604324
Sesha Mohan Srivatsava Maddali <87626654+SrivatsavaMaddali@users.noreply.github.com>
2023-07-24 22:18:07 +0530

    Update and rename NOTES.md to NOTES.py

---
+++ 0050-powx-n/NOTES.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isGood(self, nums: List[int]) -> bool:
+        l=len(nums)
+        good=[]
+        for i in range(1,l):
+            good.append(i)
+        good.append(l-1)
+        return sorted(nums)==good
+#This is the brute force approach​
