2124acc59b428b33c4a47f00d9997f5c1323d62f
Sesha Mohan Srivatsava Maddali <87626654+SrivatsavaMaddali@users.noreply.github.com>
2023-07-24 21:59:31 +0530

    Time: 76 ms (50.00%), Space: 16.4 MB (100.00%) - LeetHub

--- 2784-check-if-array-is-good/2784-check-if-array-is-good.py
+++ 2784-check-if-array-is-good/2784-check-if-array-is-good.py
@@ -1,28 +1,5 @@
 class Solution:
     def isGood(self, nums: List[int]) -> bool:
-        # l = len(nums)
-        # d = {}
-        # for x in nums:
-        #     if x not in d:
-        #         d[x] = 0
-        #     d[x] += 1
-        # print(d)
-        # if len(d)!=l-1:
-        #     return False
-        # if l==2:
-        #     if nums!=[1,1]:
-        #         return False
-        # for x in range(1, l - 1):
-        #     if x in d:
-        #         if d[x] != 1:
-        #             return False
-        #     else:
-        #         False
-        # if l-1 not in d:
-        #         d[l-1] = 0
-        # if d[l - 1] != 2:
-        #     return False
-        # return True
         l = len(nums)
         d = {}
         for x in range(1,l):
@@ -34,9 +11,9 @@ class Solution:
         print(d)
         if len(d)!=l-1:
             return False
-        if l==2:
-            if nums!=[1,1]:
-                return False
+        # if l==2:
+        #     if nums!=[1,1]:
+        #         return False
         for x in range(1, l - 1):
             if d[x]!= 1:
                 return False
