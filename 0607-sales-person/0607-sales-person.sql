c765073a2ed14cb84d123288cabd1ef2eb0cbcab
Sesha Mohan Srivatsava Maddali <87626654+SrivatsavaMaddali@users.noreply.github.com>
2023-09-11 01:35:28 +0530

    Time: 3642 ms (30.79%), Space: 0B (100.00%) - LeetHub

--- 0607-sales-person/0607-sales-person.sql
+++ 0607-sales-person/0607-sales-person.sql
@@ -7,13 +7,3 @@ select o.sales_id from Orders o where o.com_id in
 from Company c
 where c.name='RED'))
 
-
-# SELECT s.name
-# FROM SalesPerson s
-# WHERE s.sales_id NOT IN (
-#   SELECT o.sales_id
-#   FROM Orders o
-#   JOIN Company c
-#   ON c.com_id=o.com_id
-#   WHERE c.name='RED'
-# )
