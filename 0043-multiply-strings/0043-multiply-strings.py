83be1d2c544e6dcef45df0696b449b2ddebeb308
Sesha Mohan Srivatsava Maddali <87626654+SrivatsavaMaddali@users.noreply.github.com>
2023-09-16 01:38:57 +0530

    Time: 42 ms (72.19%), Space: 16.2 MB (73.60%) - LeetHub

---
+++ 0043-multiply-strings/0043-multiply-strings.py
@@ -0,0 +1,4 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        #This question is stupid. Obviously no one will ask this in interviews
+        return str(int(num1)*int(num2))
