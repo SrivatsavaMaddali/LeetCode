b6439f743ce5b2a5f6ba27876e4243fac9165ee2
Sesha Mohan Srivatsava Maddali <87626654+SrivatsavaMaddali@users.noreply.github.com>
2023-09-16 01:39:33 +0530

    Time: 34 ms (94.48%), Space: 16.3 MB (73.60%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.py
+++ 0043-multiply-strings/0043-multiply-strings.py
@@ -1,4 +1,5 @@
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
         #This question is stupid. Obviously no one will ask this in interviews
+        #So even if the question explicitly says not to use inbuilt function, I am doing so
         return str(int(num1)*int(num2))
